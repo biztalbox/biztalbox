e227d2b5fc3d63ef3351bf235930afc1fed3f0fa
biztalbox <ashish@biztalbox.com>
2025-03-19 09:15:17 +0530

    fixed service page errors

--- src/app/(service)/services/page.tsx
+++ src/app/(service)/services/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Metadata } from "next";
-import PortfolioWrapperMain from "@/pages/portfolio/portfolio-wrapper-main";
+import PortfolioWrapperMain from "@/components/portfolio/portfolio-wrapper-main";
 
 export const metadata: Metadata = {
   title: "Our Services",
