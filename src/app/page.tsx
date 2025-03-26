429b3827b1c6a4589edad226d5eeefa151eb0456
SonytatE <harshkuamr6967@gmail.com>
2025-03-26 18:33:01 +0530

    title updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { Metadata } from "next";
 import HomePage from "@/components/HomePage";
 
 export const metadata: Metadata = {
-  title: "Copy & Content Marketing Services | Engage Your Audience",
-  description: "Boost your brand's visibility with our expert copy and content marketing services. From compelling website copy to engaging blog posts, we create content that resonates and drives results.",
+  title: "Best Digital Marketing Agency | Website Development",
+  description: "Get a great website for your business! Our agency builds easy, attractive sites that help you reach more customers. Let’s create your perfect website together!",
 };
 
 export default function Home() {
