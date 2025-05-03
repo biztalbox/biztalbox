42d2ffcf0e7ddb52a20ecb346ba16bae73c23c6e
biztalbox <ashish@biztalbox.com>
2025-05-03 19:30:43 +0530

    post page footer bg-color changed

--- src/demoPages/blog/blog-details.tsx
+++ src/demoPages/blog/blog-details.tsx
@@ -63,7 +63,7 @@ const BlogDetailsMain = ({ blog }: IProps) => {
           </main>
 
           {/* footer area */}
-          <FooterFour background='dark-bg' />
+          <FooterFour />
           {/* footer area */}
         </div>
       </div>
