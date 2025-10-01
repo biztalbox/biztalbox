af357ced945f0ee6b14318fd6e756e2e8719cbf6
biztalbox <ashish@biztalbox.com>
2025-10-01 18:33:28 +0530

    clarity updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -115,7 +115,7 @@ export default function RootLayout({
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "tjbyykl95x");
+            })(window, document, "clarity", "script", "t60hghhr3a");
           `}
         </Script>
         {/* End Microsoft Clarity */}
