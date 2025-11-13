110f0a56c95132e85ca542fe699688f7508db704
biztalbox <ashish@biztalbox.com>
2025-11-13 15:52:29 +0530

    whatsapp button added

--- src/layouts/wrapper.tsx
+++ src/layouts/wrapper.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import BackToTop from "@/components/back-to-top";
+import WhatsAppButton from "@/components/whatsapp-button";
 import ThemeSetting from "@/components/theme-setting";
 if (typeof window !== "undefined") {
   require("bootstrap/dist/js/bootstrap.bundle.min");
@@ -15,6 +16,7 @@ const Wrapper = ({ children, showBackToTop=true }: WrapperProps) => {
   return (
     <React.Fragment>
       {children}
+      <WhatsAppButton />
       {showBackToTop && <BackToTop />}
       <ThemeSetting />
     </React.Fragment>
