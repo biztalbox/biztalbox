8132f3eb7b5aff2e4366785daada0741be195c6a
biztalbox <ashish@biztalbox.com>
2026-03-25 11:38:32 +0530

    background video position updated

--- src/components/hero-banner/hero-banner-four.tsx
+++ src/components/hero-banner/hero-banner-four.tsx
@@ -22,7 +22,7 @@ const styles = {
     zIndex: 0,
     pointerEvents: "none" as const,
     opacity: 0.75,
-    top: '-45px'
+    top: '-100px'
   },
   backgroundOverlay: {
     position: "absolute" as const,
