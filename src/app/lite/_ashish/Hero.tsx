2e4c211dbc80d9823a8992b8c2f8befd32d1995f
biztalbox <ashish@biztalbox.com>
2026-03-27 13:23:16 +0530

    pushed to dev

--- src/app/lite/_ashish/Hero.tsx
+++ src/app/lite/_ashish/Hero.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
     return (
         <div className="relative">
             <section id="section0" className="h-screen relative">
-                <Canvas className="!fixed top-0 h-full w-full inset-0" camera={{ position: [-4, 303, 622], fov: 70 }}>
+                <Canvas className="!fixed top-0 h-full w-full inset-0 bg-neutral-200" camera={{ position: [-4, 303, 622], fov: 70 }}>
                     <MyCanvas />
                 </Canvas>
                <div className="flex justify-btween">
